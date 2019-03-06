bb3fbf831d008afef6b4c59161612464d65ad060
clint-steve-m <clint_m@clintmejia.com>
2019-03-06 08:16:21 -0500

    Will create feed framework

---
+++ ModularFeedDemo/ModularFeedDemo/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  ModularFeedDemo
+//
+//  Created by Clint M on 3/7/19.
+//  Copyright © 2019 Clint M. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
