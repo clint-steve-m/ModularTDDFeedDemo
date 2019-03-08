6f7163e512d5852020f33475d0570a56f1849554
clint-steve-m <clint_m@clintmejia.com>
2019-03-08 10:22:15 -0500

    Will deliver invalidData error 200 HTTP response with invalid json

--- ModularFeedDemo/ModularFeedDemoTests/RemoteFeedLoaderTests.swift
+++ ModularFeedDemo/ModularFeedDemoTests/RemoteFeedLoaderTests.swift
@@ -66,6 +66,18 @@ class RemoteFeedLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        
+        let invalidJSON = Data(bytes: "invalid json".utf8)
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        
+        XCTAssertEqual(capturedErrors, [.invalidData])
+    }
+    
     
     // MARK: - Helpers
     // factory method
@@ -91,7 +103,7 @@ class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(
                 url: requestedURLs[index],
                 statusCode: code,
@@ -99,7 +111,7 @@ class RemoteFeedLoaderTests: XCTestCase {
                 headerFields: nil
             )! // hardcoded HTTPURLResponse is always valid
             
-            messages[index].completion(.success(response))
+            messages[index].completion(.success(data, response))
         }
     }
     
