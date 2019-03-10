68ab87ce306f56938b4d49780f84771a910cc5bc
clint-steve-m <clint_m@clintmejia.com>
2019-03-10 11:12:09 -0500

    Will move all mapping logic to the FeedItemsMapper

--- ModularFeedDemo/ModularFeedDemo/FeedAPI/FeedItemsMapper.swift
+++ ModularFeedDemo/ModularFeedDemo/FeedAPI/FeedItemsMapper.swift
@@ -13,7 +13,11 @@ internal final class FeedItemsMapper {
     // This item is the internal representation of a FeedItem, but for the api module.
     private struct Root: Decodable {
         let items: [Item]
-    }
+        
+        var feed: [FeedItem] {
+            return items.map { $0.item }
+        }
+     }
     
     private struct Item: Decodable {
         let id: UUID
@@ -28,12 +32,10 @@ internal final class FeedItemsMapper {
     
     private static var OK_200: Int { return 200 }
     
-    internal static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == OK_200 else {
-            throw RemoteFeedLoader.Error.invalidData
+    internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+        guard response.statusCode == OK_200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            return .failure(.invalidData)
         }
-        
-        let root = try JSONDecoder().decode(Root.self, from: data)
-        return root.items.map { $0.item }
+        return .success(root.feed)
     }
 }
