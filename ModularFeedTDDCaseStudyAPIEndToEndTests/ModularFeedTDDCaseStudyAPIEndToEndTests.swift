c0f6e82dc28353d20bb8ce355613eb85462827dd
clint-steve-m <clint_m@clintmejia.com>
2019-03-16 09:34:51 -0500

    Will extract test setup to a helper method

--- ModularFeedTDDCaseStudyAPIEndToEndTests/ModularFeedTDDCaseStudyAPIEndToEndTests.swift
+++ ModularFeedTDDCaseStudyAPIEndToEndTests/ModularFeedTDDCaseStudyAPIEndToEndTests.swift
@@ -12,20 +12,7 @@ import ModularFeedTDDCaseStudy
 class ModularFeedTDDCaseStudyAPIEndToEndTests: XCTestCase {
     
     func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
-        let testServerURL = URL(string: "https://api.myjson.com/bins/c9nia")!
-        let client = URLSessionHTTPClient()
-        let loader = RemoteFeedLoader(url: testServerURL, client: client)
-        
-        let exp = expectation(description: "Wait for load completion")
-        
-        var receivedResult: LoadFeedResult?
-        loader.load { result in
-            receivedResult = result
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 5.0)
-        
-        switch receivedResult {
+        switch getFeedResult() {
         case let .success(items)?:
             XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
             // since data is static, error can be pinpointed
@@ -50,6 +37,23 @@ class ModularFeedTDDCaseStudyAPIEndToEndTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    private func getFeedResult() -> LoadFeedResult? {
+        let testServerURL = URL(string: "https://api.myjson.com/bins/c9nia")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receivedResult: LoadFeedResult?
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
+        
+        return receivedResult
+    }
     
     private func expectedItem(at index: Int) -> FeedItem {
         return FeedItem(
