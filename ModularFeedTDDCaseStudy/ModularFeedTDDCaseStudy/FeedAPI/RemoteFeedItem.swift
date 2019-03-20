4da1f2b29826b97a9cbe0b3e584eb87914e91ba7
clint-steve-m <clint_m@clintmejia.com>
2019-03-20 12:11:58 -0500

    Will move 'RemoteFeedItem' to its own file

---
+++ ModularFeedTDDCaseStudy/ModularFeedTDDCaseStudy/FeedAPI/RemoteFeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  RemoteFeedItem.swift
+//  ModularFeedTDDCaseStudy
+//
+//  Created by Clint M on 3/22/19.
+//  Copyright © 2019 Clint M. All rights reserved.
+//
+
+import Foundation
+
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
