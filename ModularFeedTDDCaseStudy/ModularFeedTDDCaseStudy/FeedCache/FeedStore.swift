5f67a1489116347c9a108d2a18f2ba1ec08970df
clint-steve-m <clint_m@clintmejia.com>
2019-03-20 09:34:59 -0500

    Will move FeedStore' to its own file

---
+++ ModularFeedTDDCaseStudy/ModularFeedTDDCaseStudy/FeedCache/FeedStore.swift
@@ -0,0 +1,17 @@
+//
+//  FeedStore.swift
+//  ModularFeedTDDCaseStudy
+//
+//  Created by Clint M on 3/22/19.
+//  Copyright © 2019 Clint M. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
