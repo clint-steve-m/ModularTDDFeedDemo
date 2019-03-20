c1bf8edd6aad9eedc51404e1b3f3af5acb29052b
clint-steve-m <clint_m@clintmejia.com>
2019-03-20 02:23:15 -0500

    Will remove references of 'Items' in favor of 'Images' which is a domain term used by domain experts

--- ModularFeedTDDCaseStudy/ModularFeedTDDCaseStudy/FeedCache/LocalFeedImage.swift
+++ ModularFeedTDDCaseStudy/ModularFeedTDDCaseStudy/FeedCache/LocalFeedImage.swift
@@ -1,5 +1,5 @@
 //
-//  LocalFeedItem.swift
+//  LocalFeedImage.swift
 //  ModularFeedTDDCaseStudy
 //
 //  Created by Clint M on 3/22/19.
@@ -9,16 +9,16 @@
 import Foundation
 
 // Data Transfer Object - DTO
-public struct LocalFeedItem: Equatable {
+public struct LocalFeedImage: Equatable {
     public let id: UUID
     public let description: String?
     public let location: String?
-    public let imageURL: URL
+    public let url: URL
     
-    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+    public init(id: UUID, description: String?, location: String?, url: URL) {
         self.id = id
         self.description = description
         self.location = location
-        self.imageURL = imageURL
+        self.url = url
     }
 }
