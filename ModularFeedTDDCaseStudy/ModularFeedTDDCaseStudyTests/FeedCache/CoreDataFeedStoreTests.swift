88f785588ec12b9d1cbfc27f7eb881286892a4e6
clint-steve-m <clint_m@clintmejia.com>
2019-07-28 18:20:23 -0400

    Add empty CoreData feed store specs

---
+++ ModularFeedTDDCaseStudy/ModularFeedTDDCaseStudyTests/FeedCache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,61 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  ModularFeedTDDCaseStudyTests
+//
+//  Created by Clint M on 7/28/19.
+//  Copyright © 2019 Clint M. All rights reserved.
+//
+
+import XCTest
+
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        
+    }
+    
+}
