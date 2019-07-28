c541b6e743cf939c232ebf3ca9c5e3ad172e3bf7
clint-steve-m <clint_m@clintmejia.com>
2019-07-28 17:40:44 -0400

    Extract `FeedStoreSpecs` checks into reusable assertions

--- ModularFeedTDDCaseStudy/ModularFeedTDDCaseStudyTests/FeedCache/FeedStoreSpecs.swift
+++ ModularFeedTDDCaseStudy/ModularFeedTDDCaseStudyTests/FeedCache/FeedStoreSpecs.swift
@@ -9,15 +9,18 @@
 import Foundation
 
 protocol FeedStoreSpecs {
-    
     func test_retrieve_deliversEmptyOnEmptyCache()
     func test_retrieve_hasNoSideEffectsOnEmptyCache()
     func test_retrieve_deliversFoundValuesOnNonEmptyCache()
     func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
     
+    func test_insert_deliversNoErrorOnEmptyCache()
+    func test_insert_deliversNoErrorOnNonEmptyCache()
     func test_insert_overridesPreviouslyInsertedCacheValues()
     
+    func test_delete_deliversNoErrorOnEmptyCache()
     func test_delete_hasNoSideEffectsOnEmptyCache()
+    func test_delete_deliversNoErrorOnNonEmptyCache()
     func test_delete_emptiesPreviouslyInsertedCache()
     
     func test_storeSideEffects_runSerially()
@@ -35,6 +38,8 @@ protocol FailableInsertFeedStoreSpecs: FeedStoreSpecs {
 
 protocol FailableDeleteFeedStoreSpecs: FeedStoreSpecs {
     func test_delete_deliversErrorOnDeletionError()
+    func test_delete_hasNoSideEffectsOnDeletionError()
 }
 
-typealias FailableFeedStore = FailableRetrieveFeedStoreSpecs & FailableInsertFeedStoreSpecs & FailableDeleteFeedStoreSpecs
+typealias FailableFeedStoreSpecs = FailableRetrieveFeedStoreSpecs & FailableInsertFeedStoreSpecs & FailableDeleteFeedStoreSpecs
+
